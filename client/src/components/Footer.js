1ff3185fa58eb10fb2369ea23f247bb2b22b09ba
Colleen Murray <colleenkhm@gmail.com>
2022-09-15 08:30:39 -0500

    create-next-app

--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Footer() {
-    return (
-        <div className="footer">Made in 2022 by Sam Patterson and Colleen Murray</div>
-    )
-}
-
-export default Footer;
